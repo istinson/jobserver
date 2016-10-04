94f922dfac2e557e33e31a0aa021429795c79428
istinson <ian.c.stinson@gmail.com>
2016-10-04 16:29:01 -0700

    [feat] connect to db

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const api = require('./routes/controller');
+const config = require('./config');
+const mongoose = require('mongoose');
+mongoose.connect(config.getDbConnectionString(), {});
 
 app.use(express.static(`${__dirname}/public`));
 
 app.listen(port);
+console.log(`listening on port ${port}`);
---
+++ config/index.js
@@ -0,0 +1,7 @@
+const configVars = require('./configVars');
+
+module.exports = {
+	getDbConnectionString: function() {
+		return `mongodb://${configVars.user}:${configVars.pass}@ds019756.mlab.com:19756/jobwatcher`;
+	}
+};
